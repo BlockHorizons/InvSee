4ce453bae739670eddb5164c52481dfaf995e171
Muqsit <hackhack05@gmail.com>
2022-04-18 01:14:43 +0000

    Add "portable-ender-chest" module

---
+++ src/BlockHorizons/InvSee/module/PortableEnderChestModule.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BlockHorizons\InvSee\module;
+
+use BlockHorizons\InvSee\Loader;
+use BlockHorizons\InvSee\module\utils\ModuleCommand;
+use pocketmine\command\Command;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+use pocketmine\event\EventPriority;
+use pocketmine\event\inventory\InventoryCloseEvent;
+use pocketmine\player\Player;
+use pocketmine\utils\TextFormat;
+use function spl_object_id;
+use function var_dump;
+
+final class PortableEnderChestModule implements Module, CommandExecutor{
+
+	public static function fromConfiguration(array $configuration) : Module{
+		return new self(
+			ModuleCommand::parse($configuration["command"], [
+				"usage" => "/{$configuration["command"]["name"]} - Access your ender chest",
+				"permission" => ["description" => "Grants permission to /{$configuration["command"]["name"]} command"]
+			])
+		);
+	}
+
+	private Loader $loader;
+
+	/** @var array<string, int> */
+	private array $viewing = [];
+
+	public function __construct(
+		private ModuleCommand $command
+	){}
+
+	public function onEnable(Loader $loader) : void{
+		$this->command->setup($loader, $this);
+		$this->loader = $loader;
+
+		$loader->getServer()->getPluginManager()->registerEvent(InventoryCloseEvent::class, function(InventoryCloseEvent $event) : void{
+			if(isset($this->viewing[$uuid = $event->getPlayer()->getUniqueId()->getBytes()]) && $this->viewing[$uuid] === spl_object_id($event->getInventory())){
+				unset($this->viewing[$uuid]);
+			}
+		}, EventPriority::MONITOR, $loader);
+	}
+
+	public function onDisable(Loader $loader) : void{
+		$this->command->destroy($loader);
+
+		$server = $loader->getServer();
+		foreach($this->viewing as $uuid => $_){
+			$server->getPlayerByRawUUID($uuid)?->removeCurrentWindow();
+		}
+		$this->viewing = [];
+	}
+
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+		if(!($sender instanceof Player)){
+			$sender->sendMessage(TextFormat::RED . "This command can only be used by a player");
+			return true;
+		}
+
+		if(isset($this->viewing[$uuid = $sender->getUniqueId()->getBytes()])){
+			return true;
+		}
+
+		$menu = $this->loader->getPlayerList()->getOrCreate($sender->getName())->getEnderChestInventoryMenu();
+		$this->viewing[$uuid] = spl_object_id($menu->getInventory());
+		$menu->send($sender, "Ender Chest", function(bool $success) use($uuid) : void{
+			if(!$success){
+				unset($this->viewing[$uuid]);
+			}
+		});
+		return true;
+	}
+}
---
+++ src/BlockHorizons/InvSee/module/utils/ModuleCommand.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BlockHorizons\InvSee\module\utils;
+
+use BlockHorizons\InvSee\Loader;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\PluginCommand;
+use pocketmine\permission\Permission;
+use pocketmine\permission\PermissionManager;
+use RuntimeException;
+use function current;
+use function implode;
+
+final class ModuleCommand{
+
+	/**
+	 * @param mixed[] ...$configurations
+	 * @return self
+	 *
+	 * @phpstan-param array<string, mixed> ...$configurations
+	 */
+	public static function parse(array ...$configurations) : self{
+		$get_config = static function(string ...$identifier) use($configurations) : mixed{
+			foreach($configurations as $configuration){
+				$value = $configuration;
+				reset($identifier);
+				while(($entry = current($identifier)) !== false){
+					if(isset($value[$entry])){
+						$value = $value[$entry];
+						next($identifier);
+					}else{
+						continue 2;
+					}
+				}
+				if(current($identifier) === false){
+					return $value;
+				}
+			}
+			throw new RuntimeException("Config identifier " . implode(".", $identifier) . " is not set");
+		};
+
+		return new self(
+			$get_config("name"),
+			$get_config("usage"),
+			$get_config("permission", "name"),
+			$get_config("permission", "description"),
+			$get_config("permission", "access"),
+			$get_config("aliases")
+		);
+	}
+
+	private ?PluginCommand $command = null;
+	private ?Permission $permission = null;
+
+	/**
+	 * @param string $name
+	 * @param string $usage
+	 * @param string $permission_name
+	 * @param string $permission_description
+	 * @param string $permission_accessibility
+	 * @param string[] $aliases
+	 */
+	public function __construct(
+		private string $name,
+		private string $usage,
+		private string $permission_name,
+		private string $permission_description,
+		private string $permission_accessibility,
+		private array $aliases
+	){}
+
+	public function getName() : string{
+		return $this->name;
+	}
+
+	public function setup(Loader $loader, CommandExecutor $executor) : PluginCommand{
+		// Permission registration
+		$permission_manager = PermissionManager::getInstance();
+		$command_permission = new Permission($this->permission_name, $this->permission_description);
+		if(!$permission_manager->addPermission($command_permission)){
+			throw new RuntimeException("Permission {$command_permission->getName()} is already registered");
+		}
+		ModuleUtils::assignPermissionDefault($command_permission, $this->permission_accessibility);
+		$this->permission = $command_permission;
+
+		// Command registration
+		$command_manager = $loader->getServer()->getCommandMap();
+		$command = new PluginCommand($this->name, $loader, $executor);
+		$command->setPermission($command_permission->getName());
+		$command->setUsage($this->usage);
+		$command->setAliases($this->aliases);
+		$command_manager->register($loader->getName(), $command);
+		$this->command = $command;
+		return $command;
+	}
+
+	public function destroy(Loader $loader) : void{
+		PermissionManager::getInstance()->removePermission($this->permission ?? throw new RuntimeException("Cannot retrieve permission: {$this->permission_name}"));
+		$this->permission = null;
+
+		$loader->getServer()->getCommandMap()->unregister($this->command ?? throw new RuntimeException("Cannot retrieve command: /{$this->name}"));
+		$this->command = null;
+	}
+}
